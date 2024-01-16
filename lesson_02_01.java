24b6c0d556936b7557c6922689638320a8e2afd9
ColotVS <kolotilov98@mail.ru>
2024-01-16 16:58:49 +0700

    Тип int Java Урок 2, задача 1

---
+++ lesson_02_01.java
@@ -0,0 +1,10 @@
+public class lesson_02_01 {
+    public static void main(String[] args) {
+        /*В методе main объяви переменные intValue, numericValue, size, number типа int.
+Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+Значениями могут быть любые целые числа.*/
+        int intValue = 5, numericValue = 3, size = 10, number = 15;
+        System.out.println(intValue + numericValue + size + number);
+
+    }
+}
