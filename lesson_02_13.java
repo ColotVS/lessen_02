f54af6aa1e4e1f06ed6426b08111e533f873b469
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:32:52 +0700

    Метод toUpperCse класса String Урок 2, задача 13

---
+++ lesson_02_13.java
@@ -0,0 +1,12 @@
+public class lesson_02_13 {
+    public static void main(String[] args) {
+        /*В методе main на экран выводятся три строки.
+        Внеси изменения в код, чтобы все буквы в этих строках стали заглавными.*/
+        String caps = "if I type in caps ";
+        String usa = "сша";
+
+        System.out.println(usa.toUpperCase());
+        System.out.println("Винни Пух".toUpperCase());
+        System.out.println(caps.toUpperCase() + "they know I mean business".toUpperCase());
+    }
+}
