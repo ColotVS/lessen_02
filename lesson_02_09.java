6b3b6a50d30651b6683f0afa03bb033962c7ee6e
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:22:21 +0700

    Конкатенация строк и чисел Урок 2, задача 9

---
+++ lesson_02_09.java
@@ -0,0 +1,13 @@
+public class lesson_02_09 {
+    public static void main(String[] args) {
+        /*В переменную digits нужно записать строку "60".
+                Используй переменные x, y, z, пустую строку и конкатенацию строк.*/
+        int x = 2;
+        int y = 4;
+        int z = 0;
+
+        String digits = x+y+""+z;
+
+        System.out.println(digits);
+    }
+}
