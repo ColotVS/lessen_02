254a0f20ca1a573bcd2d83b5e95375e7e5eef81d
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:20:45 +0700

    Конкатенация строк 2 Урок 2, задача 8

---
+++ lesson_02_08.java
@@ -0,0 +1,10 @@
+public class lesson_02_08 {
+    public static void main(String[] args) {
+        /*Заполни пробелы пустотой... или наоборот.
+        Используй переменную emptiness, строки с одним пробелом и конкатенацию строк,
+        чтобы записать в переменную fullness строку "пустота пустота пустота".*/
+        String emptiness = "пустота";
+        String fullness = emptiness + " " + emptiness + " " + emptiness; //напишите тут ваш код
+        System.out.println(fullness);
+    }
+}
