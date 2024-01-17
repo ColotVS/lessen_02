f336d501724b74c4c4a2e771ca5a058313a57fbb
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:28:39 +0700

    Метод length класса String Урок 2, задача 11

---
+++ lesson_02_11.java
@@ -0,0 +1,10 @@
+public class lesson_02_11 {
+    public static void main(String[] args) {
+        /*В методе main на экран выводятся значения трех строк.
+                Внеси изменения в код, чтобы вместо значений строк вывелась длина каждой строки.*/
+        String emptyString = "";
+        System.out.println(emptyString.length());
+        System.out.println("Gomu Gomu no Bazooka!".length());
+        System.out.println((emptyString + 2 + 2 + "22").length());
+    }
+}
