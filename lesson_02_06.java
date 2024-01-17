4f1485b74501c079b44040c1ad68e96ec5a52b65
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:16:46 +0700

    Тип String Урок 2, задача 6

---
+++ lesson_02_06.java
@@ -0,0 +1,9 @@
+public class lesson_02_06 {
+    public static void main(String[] args) {
+
+        /*В методе main объяви переменные word, phrase, line и text типа String.
+        Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+        Значениями могут быть любые строки.*/
+        String word="word", phrase ="phrase", line ="line", text ="text";
+    }
+}
