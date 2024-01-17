d4b5d4e7f8b42237e13df91d3881a1025d0d4a0b
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:10:15 +0700

    Сантиметры в метры Урок 2, задача 2

---
+++ lesson_02_02.java
@@ -0,0 +1,13 @@
+public class lesson_02_02 {
+    public static void main(String[] args) {
+        /*В переменной centimeters записано количество сантиметров.
+В переменную meters нужно записать количество полных метров в centimeters (1 метр = 100 см).
+Для вычисления используй переменную centimeters и оператор деления.
+Для объявления и инициализации meters используй одну команду.*/
+
+        int centimeters = 243;
+        int meters = centimeters/100;
+
+        System.out.println(meters);
+    }
+}
