61628c5596e34b464bd92ec8392e7c79f1e70b8f
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:14:00 +0700

    Инкремент Урок 2, задача 4

---
+++ lesson_02_04.java
@@ -0,0 +1,10 @@
+public class lesson_02_04 {
+    public static void main(String[] args) {
+        /*Используя только оператор инкремента, измени значение в переменной six, чтобы на экран вывелась цифра 9.*/
+        int six = 6;
+        six++;
+        six++;
+        six++;
+        System.out.println(six);
+    }
+}
