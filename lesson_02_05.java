76bf1a720930bb659fb2fc2c7024c7db904044cc
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:15:17 +0700

    Декремент Урок 2, задача 5

---
+++ lesson_02_05.java
@@ -0,0 +1,20 @@
+public class lesson_02_05 {
+    public static void main(String[] args) {
+
+        /*Используя только оператор декремента, измени значение переменной toothCounter, чтобы на экран вывелось число 23.*/
+        int toothCounter = 32;
+
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+
+
+        System.out.println(toothCounter);
+    }
+}
