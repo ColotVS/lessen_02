f3cfa0da541190de0d015a6f6aa0a8bf48df1937
ColotVS <kolotilov98@mail.ru>
2024-01-17 12:19:21 +0700

    Конкатенация строк Урок 2, задача 7

---
+++ lesson_02_07.java
@@ -0,0 +1,13 @@
+public class lesson_02_07 {
+    public static void main(String[] args) {
+        /*В переменную tagline нужно записать строку "JustDoIt".
+                Используй s1, s2, s3 и конкатенацию строк.*/
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String tagline = s3+s1+s2;
+
+        System.out.println(tagline);
+    }
+}
